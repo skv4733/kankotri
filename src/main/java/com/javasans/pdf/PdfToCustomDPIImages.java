e9aa7f671511c5d1008bc755eaa59e70afe7db50
Sandip Vaghasiya <skv4733@gmail.com>
2020-11-18 21:27:50 -0600

    Update PdfToCustomDPIImages.java

--- src/main/java/com/javasans/pdf/PdfToCustomDPIImages.java
+++ src/main/java/com/javasans/pdf/PdfToCustomDPIImages.java
@@ -8,27 +8,43 @@ import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+/**
+ *
+ */
 public class PdfToCustomDPIImages {
 
-    public static void main(String[] args) throws Exception {
-        PdfToCustomDPIImages main = new PdfToCustomDPIImages();
 
-        File file = new File("C:\\Users\\Super User\\IdeaProjects\\apachePDFbox\\inputfiles");
+    public static void main(String[] args) {
+        new PdfToCustomDPIImages().convert();
+    }
 
-        if (file.isDirectory()) {
+    public void convert() {
 
-            File[] files = file.listFiles();
-            for (File file1 : files) {
-                String name = file1.getName();
-                String replace1 = file1.getPath().replace(".pdf", "compressed.pdf");
-                String replace = name.replace(".pdf", "compressed.pdf");
-                System.out.println("name = " + replace);
-                main.convertPdftocustomDPIImages(file1.toPath().toString(),replace);
+        try {
+            PdfToCustomDPIImages main = new PdfToCustomDPIImages();
+            Path photos = Paths.get("photos").toAbsolutePath();
+            File file = photos.toFile();
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                for (File file1 : files) {
+                    String name = file1.getName();
+                    String replace1 = file1.getPath().replace(".pdf", "compressed.pdf");
+                    String replace = name.replace(".pdf", "compressed.pdf");
+                    System.out.println("name = " + replace);
+                    main.convertPdftocustomDPIImages(file1.toPath().toString(), replace);
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
+
 
     public void convertPdftocustomDPIImages(String inputfilename, String outputimagefilename) throws IOException {
         String filename = inputfilename;
@@ -37,12 +53,11 @@ public class PdfToCustomDPIImages {
         PDFRenderer pdfRenderer = new PDFRenderer(document);
         for (int page = 0; page < document.getNumberOfPages(); ++page) {
             BufferedImage bim = pdfRenderer.renderImageWithDPI(
-                    page, 150, ImageType.RGB);
+                page, 150, ImageType.RGB);
             ImageIOUtil.writeImage(
-                    bim, String.format(outputimagefilename+"pdf-%d.%s", page + 1, extension), 300);
+                bim, String.format(outputimagefilename + "pdf-%d.%s", page + 1, extension), 300);
         }
         document.close();
     }
-
 
 }
