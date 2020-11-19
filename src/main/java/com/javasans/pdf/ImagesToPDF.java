8910df7c22b9646db9531bd90be01a33b5a26b9e
Sandip Vaghasiya <skv4733@gmail.com>
2020-11-18 21:28:30 -0600

    Update ImagesToPDF.java

--- src/main/java/com/javasans/pdf/ImagesToPDF.java
+++ src/main/java/com/javasans/pdf/ImagesToPDF.java
@@ -9,9 +9,13 @@ import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ImagesToPDF {
-    public void imagesToPdf(String destination, String pdfName, String imagFileSource) throws IOException, DocumentException {
+    public void imagesToPdf(String destination, String pdfName, File[] splitImagFiles) throws IOException, DocumentException {
 
         Document document = new Document(PageSize.A4, 20.0f, 20.0f, 20.0f, 150.0f);
         String desPath = destination;
@@ -35,10 +39,8 @@ public class ImagesToPDF {
 
         System.out.println("CONVERTER START.....");
 
-        String[] splitImagFiles = imagFileSource.split("@@");
-
-        for (String singleImage : splitImagFiles) {
-            Image image = Image.getInstance(singleImage);
+        for (File singleImage : splitImagFiles) {
+            Image image = Image.getInstance(singleImage.getAbsolutePath());
             document.setPageSize(image);
             document.newPage();
             image.setAbsolutePosition(0, 0);
@@ -51,17 +53,40 @@ public class ImagesToPDF {
     }
 
 
-    public static void main(String[] args) throws IOException, DocumentException {
+    public static void main(String[] args) {
+        ImagesToPDF imagesToPDF = new ImagesToPDF();
+        imagesToPDF.run();
+    }
 
+    private void run() {
+        try {
 
-        /*output file name*/
-        String name = "riddhi_birth_certificate";
-        String destination = "C:\\desti";
-        String path1 = "C:\\kankori\\prepared\\2.jpg";
-        String path2 = "C:\\kankori\\prepared\\1.jpg";
-//        String path3 = "C:\\kankori\\prepared\\3.jpg";
-        String sourcePath4 = path1 + "@@" + path2 ;
-        new ImagesToPDF().imagesToPdf(destination, name, sourcePath4);
+            Path inputFolder = Paths.get("fileTobeMergedWIthSubfolder").toAbsolutePath();
+            Path merged = Paths.get("merged").toAbsolutePath();
+            FileUtils.validateIfFolderExist(merged);
+            File file = inputFolder.toFile();
+            if (file.isDirectory()) {
+                for (File subDir : file.listFiles()) {
+                    if (subDir.isDirectory()) {
+                        String collect = Arrays.stream(subDir.list()).collect(Collectors.joining("@@"));
+                        imagesToPdf(merged+File.separator, subDir.getName(), subDir.listFiles());
+                    }
+                }
+            }
+//
+//            /*output file name*/
+//            String name = "3e3e33";
+//            String destination = "C:\\desti";
+//            String path1 = "C:\\kankori\\prepared\\2.jpg";
+//            String path2 = "C:\\kankori\\prepared\\1.jpg";
+////        String path3 = "C:\\kankori\\prepared\\3.jpg";
+//            String sourcePath4 = path1 + "@@" + path2 ;
+//            imagesToPdf(destination, name, sourcePath4);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
 
     }
 
